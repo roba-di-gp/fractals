74edd3599bfd7f9216217b3ac29b8ef4ee0fd258
Guglielmo Pellitteri <67461869+lateral-gug@users.noreply.github.com>
2020-08-27 17:34:56 +0200

    Add files via upload

---
+++ mandelbrot.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cmath
+import time
+start = time.time()
+
+nx = 900
+ny = 600
+iters = 25
+
+def f(z,c):
+    return z*z + c
+
+x = np.linspace(-2,1,nx)
+y = np.linspace(-1,1,ny)
+xx = np.array([])
+yy = np.array([])
+
+for i in range(nx):
+    for j in range(ny):
+        z = complex(0,0)
+        for n in range(iters):
+            if abs(z) < 2:
+                z = f(z,complex(x[i],y[j]))
+            else: break
+        if abs(z) < 2:
+            xx = np.append(xx,x[i])
+            yy = np.append(yy,y[j])
+    if i % 10 == 0:
+        print('%.2f'%(i/nx))
+
+plt.errorbar(xx,yy,color='black',fmt='.',markersize=0.32)
+
+print('\n')
+print(time.time() - start)
+plt.show()
